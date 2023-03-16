83b79c5cba468bb64e2a64a4d405c706f94b3cd1
akuku20 <alicjakuk20@gmail.com>
2023-03-16 21:24:05 +0100

    Initial Commit

---
+++ TriviaChallenge/TriviaChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TriviaChallengeApp.swift
+//  TriviaChallenge
+//
+//  Created by Alicja on 16/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct TriviaChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
